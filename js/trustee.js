4ebc642f94c9745767d729be0aac31c68f4c3202
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-08-17 12:23:57 +0600

    sorted member page created

--- js/trustee.js
+++ js/trustee.js
@@ -1,10 +1,15 @@
+let memberData = [];
 const trusteeMembersData = () => {
   fetch("js/trustee.json")
     .then((res) => res.json())
-    .then((data) => setTrusteeMembersData(data.trusteeData));
+    .then((data) => {
+      setTrusteeMembersData(data.trusteeData);
+      // console.log(data.trusteeData);
+    });
 };
 
 const setTrusteeMembersData = (data) => {
+  memberData = [...data];
   const trusteeContainer = document.getElementById("trustee-container");
   data.slice(0, 37).forEach((element) => {
     const memberDiv = document.createElement("div");
@@ -13,10 +18,20 @@ const setTrusteeMembersData = (data) => {
     <div>
     <img onclick = "openPhotoDetails('${element.image}', '${element.name_english}', '${element.category}')" style="margin-bottom: 20px;" src="${element.image}" class="img-thumbnail img-fluid"
 						alt="">
-    
     `;
     trusteeContainer.appendChild(memberDiv);
   });
+};
+
+// filtering died trustee members information
+const filteringRememberData = () => {
+  const filteredData = memberData.filter(
+    (member) => member.category === "Trustee Remember"
+  );
+  // console.log(filteredData);
+  const encodedData = encodeURIComponent(JSON.stringify(filteredData));
+  // console.log(encodedData);
+  window.location.href = `we-remember.html?data=${encodedData}`;
 };
 
 const memberContainer = document.getElementById("member-container");
